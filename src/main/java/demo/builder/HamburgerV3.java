3a1432fb48f482c87ce5625f81c5e7e45d2745da
JoeGukseon <zipcks1381@gmail.com>
2023-07-01 13:42:59 +0900

    빌더버전 필수값 처리 추가

--- src/main/java/demo/builder/HamburgerV3.java
+++ src/main/java/demo/builder/HamburgerV3.java
@@ -7,12 +7,16 @@ Builder 버전
  */
 @Builder
 @ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class HamburgerV3 {
     // 필수 파라미터
-    private int bun;
-    private int patty;
+    private final int bun;
+    private final int patty;
+
+    //필수값인 경우 빌더를 재정의 하여 빌더의 생성자로 필수값을 받아준다.
+    public static HamburgerV3Builder builder(int bun,int patty) {
+        return new HamburgerV3Builder().bun(bun).patty(patty);
+    }
 
     // 선택 매개변수
     private int cheese;
