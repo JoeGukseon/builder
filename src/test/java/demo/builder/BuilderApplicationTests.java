aa8e836478aefff0e96508f8dbf9722bf5fc4541
JoeGukseon <zipcks1381@gmail.com>
2023-07-01 13:04:16 +0900

    자바 빌더 버전 테스트 추가

--- src/test/java/demo/builder/BuilderApplicationTests.java
+++ src/test/java/demo/builder/BuilderApplicationTests.java
@@ -1,7 +1,6 @@
 package demo.builder;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class BuilderApplicationTests {
@@ -9,23 +8,23 @@ class BuilderApplicationTests {
     @Test
     void basic() {
         // 모든 재료가 있는 햄버거
-        HamburgerBasic hamburgerBasic1 = new HamburgerBasic(2, 1, 2, 4, 6, 8);
+        HamburgerV1 hamburger1 = new HamburgerV1(2, 1, 2, 4, 6, 8);
 
         // 빵과 패티 치즈만 있는 햄버거
-        HamburgerBasic hamburgerBasic2 = new HamburgerBasic(2, 1, 1);
+        HamburgerV1 hamburger2 = new HamburgerV1(2, 1, 1);
 
         // 빵과 패티 베이컨만 있는 햄버거
-        HamburgerBasic hamburgerBasic3 = new HamburgerBasic(2, 0);
+        HamburgerV1 hamburger3 = new HamburgerV1(2, 0);
 
-        System.out.println("hamburger1 = " + hamburgerBasic1);
-        System.out.println("hamburger2 = " + hamburgerBasic2);
-        System.out.println("hamburger3 = " + hamburgerBasic3);
+        System.out.println("hamburger1 = " + hamburger1);
+        System.out.println("hamburger2 = " + hamburger2);
+        System.out.println("hamburger3 = " + hamburger3);
     }
 
     @Test
     void javaBean() {
         // 모든 재료가 있는 햄버거
-        HamburgerJavaBean hamburger1 = new HamburgerJavaBean();
+        HamburgerV2 hamburger1 = new HamburgerV2();
         hamburger1.setBun(2);
         hamburger1.setPatty(1);
         hamburger1.setCheese(2);
@@ -34,14 +33,14 @@ class BuilderApplicationTests {
         hamburger1.setBacon(8);
 
         // 빵과 패티 치즈만 있는 햄버거
-        HamburgerJavaBean hamburger2 = new HamburgerJavaBean();
+        HamburgerV2 hamburger2 = new HamburgerV2();
         hamburger2.setBun(2);
         hamburger2.setPatty(1);
         hamburger2.setCheese(1);
 
 
         // 빵과 패티 베이컨만 있는 햄버거
-        HamburgerJavaBean hamburger3 = new HamburgerJavaBean();
+        HamburgerV2 hamburger3 = new HamburgerV2();
         hamburger3.setBun(2);
         hamburger3.setPatty(1);
 
@@ -50,4 +49,30 @@ class BuilderApplicationTests {
         System.out.println("hamburger3 = " + hamburger3);
     }
 
+    @Test
+    void builder() {
+        HamburgerV3 hamburger1 = HamburgerV3.builder()
+                .bun(2)
+                .patty(1)
+                .cheese(2)
+                .lettuce(4)
+                .tomato(6)
+                .bacon(8)
+                .build();
+
+        HamburgerV3 hamburger2 = HamburgerV3.builder()
+                .bun(2)
+                .patty(1)
+                .cheese(2)
+                .build();
+
+        HamburgerV3 hamburger3 = HamburgerV3.builder()
+                .bun(2)
+                .patty(1)
+                .build();
+
+        System.out.println("hamburger1 = " + hamburger1);
+        System.out.println("hamburger2 = " + hamburger2);
+        System.out.println("hamburger3 = " + hamburger3);
+    }
 }
